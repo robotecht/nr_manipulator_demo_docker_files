cebb0ee0908e89f5d451d8702c616c1f11d35be5
coenadasNR <coenadas.nr@gmail.com>
2024-10-03 14:11:44 +0100

    added gui

---
+++ demo_pick_and_place/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import subprocess
+from subprocess import Popen, PIPE
+from subprocess import check_output
+
+
+def pick_place():
+    subprocess.call("run.sh", shell=True)
+
+app = Flask(__name__)
+
+@app.route('/')
+def run_demo():
+    return render_template('index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+# @app.route('/start_demo', methods=['POST','GET'])
+# def start_demo():
+#     return 'running'
+
+@app.route('/start_demo',methods=['POST','GET'])
+def home():
+    pick_place()
+    return 'running'
